70a2688d9169dee4eaa884ee3bef698eb80f103b
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-25 21:01:59 +0400

    navbaritem calls action sheet and from there user can choose to go either apple.com or hackingswift.com

--- browserApp/ViewController.swift
+++ browserApp/ViewController.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
 
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(buttonTapped))
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        let request = URLRequest(url: url)
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
+        
+        
+        
     }
+    
+    @objc func buttonTapped() {
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac,animated: true)
+
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
 
 
 }
